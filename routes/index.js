30c8d3ffeb852f2c52cf64a6260d4ceb6ce35436
adhilameenet <adhilameenet@gmail.com>
2022-04-16 19:47:49 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+router.post('/', (req, res, next) => {
+  const Client = require('twitter.js').Client
+  const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAC8XbgEAAAAAKFiNVb6448sP7fgLKcbOCdFc%2FNU%3DnQcsjKmMjPxw6iapfVTMNC0yfYQRyD4XodjSCRvRRFIMdVXfpd';
+  const client = new Client();
+  client.loginWithBearerToken(bearerToken);
+  
+  const username = req.body.username;
+  
+  client.users.fetchByUsername(username).then((user) => {
+    if (user) {
+      res.render('user.hbs', { user })
+    }
+  });
+})
+
+
+module.exports = router;

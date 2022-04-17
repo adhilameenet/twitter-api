0c36f413da8e0d784c508dadea977aab01997653
adhilameenet <adhilameenet@gmail.com>
2022-04-17 09:30:05 +0530

    some changes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 var express = require('express');
 const process = require('dotenv').config()
 var router = express.Router();
-console.log(process.parsed)
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
 });
 router.post('/', (req, res, next) => {
-  const http = require('http')
   const Client = require('twitter.js').Client
-  
   const bearerToken = process.parsed.BEARER_TOKEN ;
-  
   const client = new Client();
   client.loginWithBearerToken(bearerToken);
   const username = req.body.username;
